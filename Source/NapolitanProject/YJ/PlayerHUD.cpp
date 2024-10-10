c498e4449fb850159ba5ab454cf15cdcbab5be04
viabe <98133327+viabe@users.noreply.github.com>
2024-10-10 18:35:16 +0900

    [feat] 코드 업로드

--- Source/NapolitanProject/YJ/PlayerHUD.cpp
+++ Source/NapolitanProject/YJ/PlayerHUD.cpp
@@ -4,6 +4,7 @@
 #include "PlayerHUD.h"
 #include "InventoryWidget.h"
 #include "Blueprint/UserWidget.h"
+#include "NYS_Choice.h"
 
 void APlayerHUD::BeginPlay()
 {
@@ -18,5 +19,15 @@ void APlayerHUD::BeginPlay()
 
 	// 초반에 slot 을 생성해주기
 	
+
+	if (!NYSFactory)
+		return;
+
+	NYSUi = CastChecked<UNYS_Choice>(CreateWidget(GetWorld(), NYSFactory));
+
+	if (NYSUi) {
+		NYSUi->AddToViewport();
+		NYSUi->SetVisibility(ESlateVisibility::Hidden);
+	}
 	
 }
