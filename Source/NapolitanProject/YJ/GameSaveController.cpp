f2445dc5a4b57c1538158c0a48793acbb6904d1d
silverlnng <112385982+silverlnng@users.noreply.github.com>
2024-10-14 21:19:51 +0900

    저장슬롯 만들기 중

---
+++ Source/NapolitanProject/YJ/GameSaveController.cpp
@@ -0,0 +1,14 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "GameSaveController.h"
+
+void UGameSaveController::SaveGame(FVector PlayerPosition, FString LevelName, FString SlotName)
+{
+	
+}
+
+bool UGameSaveController::LoadGame(FVector& OutPlayerPosition, FString& OutLevelName, FString SlotName)
+{
+	return true;
+}
