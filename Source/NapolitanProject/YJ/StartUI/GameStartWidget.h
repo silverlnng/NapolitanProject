b3abc73afc17959eccf3a774b518675505dd29e6
silverlnng <112385982+silverlnng@users.noreply.github.com>
2024-10-15 19:17:44 +0900

    GameStartUI 만들기

---
+++ Source/NapolitanProject/YJ/StartUI/GameStartWidget.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "GameStartWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class NAPOLITANPROJECT_API UGameStartWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
