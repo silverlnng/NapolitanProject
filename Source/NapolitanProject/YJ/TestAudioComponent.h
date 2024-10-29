75309fef251405f1177c0bfcdad78c5c3b9335e3
silverlnng <112385982+silverlnng@users.noreply.github.com>
2024-10-29 16:14:52 +0900

    수첩 획득

---
+++ Source/NapolitanProject/YJ/TestAudioComponent.h
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/AudioComponent.h"
+#include "TestAudioComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class NAPOLITANPROJECT_API UTestAudioComponent : public UAudioComponent
+{
+	GENERATED_BODY()
+public:
+
+};
