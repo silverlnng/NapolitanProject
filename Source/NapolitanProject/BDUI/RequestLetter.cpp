451fb635299e455a8cea40034ee5201a2409c744
viabe <skqldhkd123@naver.com>
2024-11-24 15:11:30 +0900

    열쇠/편지 에셋 업뎃, 스타트 시 사무실로 가도록 연결, 사무실에서 의뢰서 티켓 부분을 눌렀을 시 미술관 레벨로 이동

--- Source/NapolitanProject/BDUI/RequestLetter.cpp
+++ Source/NapolitanProject/BDUI/RequestLetter.cpp
@@ -3,3 +3,17 @@
 
 #include "RequestLetter.h"
 
+#include "Components/Button.h"
+#include "Kismet/GameplayStatics.h"
+
+void URequestLetter::NativeConstruct()
+{
+	Super::NativeConstruct();
+	Button_Ticket->OnClicked.AddDynamic(this, &URequestLetter::OnTicketClicked);
+	
+}
+
+void URequestLetter::OnTicketClicked()
+{
+	UGameplayStatics::OpenLevelBySoftObjectPtr(this,MuseumLevel,true);
+}
