905d43bdba9551e29d91bea7084326d9b41ed4c4
viabe <skqldhkd123@naver.com>
2025-02-18 00:00:31 +0900

    문 업뎃

--- Source/NapolitanProject/Interact/FashionDoor.cpp
+++ Source/NapolitanProject/Interact/FashionDoor.cpp
@@ -3,12 +3,16 @@
 
 #include "FashionDoor.h"
 
+#include "Components/BoxComponent.h"
+#include "Kismet/GameplayStatics.h"
+#include "NapolitanProject/GameFrameWork/TestCharacter.h"
+
 // Sets default values
 AFashionDoor::AFashionDoor()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
-
+	BoxComp->OnComponentBeginOverlap.AddDynamic(this, &AFashionDoor::OnBeginOverlap);
 }
 
 // Called when the game starts or when spawned
@@ -24,4 +28,40 @@ void AFashionDoor::Tick(float DeltaTime)
 	Super::Tick(DeltaTime);
 
 }
+
+void AFashionDoor::RotateDoor()
+{
+	Super::RotateDoor();
+}
+
+void AFashionDoor::OnBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	//만약 부딪힌게 캐릭터일 경우 Frame은 아래로 내려감
+	if(OtherActor)
+	{
+		if(bIsOpenDoor) // 한번만 열리도록 만든 것
+			return;
+		
+		auto* Target = Cast<ATestCharacter>(OtherActor);
+		if(Target)
+		{
+			bIsOpenDoor = true;
+			if (OpenDoorSound)
+			{
+				UGameplayStatics::PlaySound2D(GetWorld(), OpenDoorSound); // 소리 재생
+			}
+			
+			// 목표 Yaw 계산 (현재 Yaw에서 90도 추가)
+			FRotator CurrentRotation = ExitDoor->GetRelativeRotation();
+			TargetYaw = CurrentRotation.Yaw + 90.0f;
+
+			// 타이머 설정: 문을 부드럽게 회전
+			GetWorld()->GetTimerManager().SetTimer(TimerHandle, this, &AExitDoorTrue::RotateDoor, 0.01f, true);
+		}
+	}
+
+}
+
+
 
--- Source/NapolitanProject/Interact/FashionDoor.h
+++ Source/NapolitanProject/Interact/FashionDoor.h
@@ -24,4 +24,10 @@ public:
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
+public:
+	virtual void RotateDoor() override;
+
+	virtual void OnBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult) override;
+
+	
 };
