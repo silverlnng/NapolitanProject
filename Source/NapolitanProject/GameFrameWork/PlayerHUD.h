99ed78c1ffbace201579c891d526908322f96e32
yjkwak <silverlnng0721@gmail.com>
2025-05-02 15:13:58 +0900

    노트 닫기버튼만들기, 아이템소리 넣는중

--- Source/NapolitanProject/GameFrameWork/PlayerHUD.h
+++ Source/NapolitanProject/GameFrameWork/PlayerHUD.h
@@ -18,12 +18,21 @@ public:
 
 	UPROPERTY(VisibleAnywhere)
 	class UMyTestGameInstance* GI;
+	
+	class ATestPlayerController* PC;
+
+	class ATestCharacter* MainCharacter;
 
 	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = UI)
 	TSubclassOf<class UNoteWidget> NoteWidgetFactory;
 	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = UI)
 	class UNoteWidget* NoteUI;
+	UFUNCTION()
+	void OnClickBtn_NoteClose();
 
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Sound")
+	USoundWave* CloseSoundWave;
+	
 	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = UI)
 	TSubclassOf<class UNPCDialogueWidget> NPCDialogueWidgetFactory;
 	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = UI)
