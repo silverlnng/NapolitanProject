a3f1486ec9b0726e88638f834c13a381b8969ca3
yjkwak <silverlnng0721@gmail.com>
2025-05-23 21:38:54 +0900

    스카프import , 영수 4번 선택시 보임.

--- Source/NapolitanProject/NPC/NPC_Youngsoo.cpp
+++ Source/NapolitanProject/NPC/NPC_Youngsoo.cpp
@@ -202,7 +202,7 @@ void ANPC_Youngsoo::ResultEvent(int32 result)
 		else
 		{
 			//"위대한 빨간 등대를 부수자" 선택 후
-			MainCharacter->GetMesh()->SetHiddenInGame(true);
+			MainCharacter->GetMesh()->SetHiddenInGame(false);
 			TestPC->CameraViewChangePlayer();
 			//위대한 빨간 등대를 부수자 -> 카메라 시점 변경 및 플레이어 얼굴을 클로즈 업
 			MainCharacter->AdjustCameraPosition(); //시점 변경
