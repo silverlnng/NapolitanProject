089c1cdddda9889f5ad7a831938a1c604022b85b
yjkwak <silverlnng0721@gmail.com>
2025-07-25 13:32:17 +0900

    플레이어 controller rotation yaw 부분 삭제

--- Source/NapolitanProject/NPC/Curator/Command/CuratorCompletedCommand.cpp
+++ Source/NapolitanProject/NPC/Curator/Command/CuratorCompletedCommand.cpp
@@ -3,7 +3,12 @@
 
 #include "CuratorCompletedCommand.h"
 
+#include "NapolitanProject/GameFrameWork/PlayerHUD.h"
+#include "NapolitanProject/GameFrameWork/TestCharacter.h"
+#include "NapolitanProject/GameFrameWork/TestPlayerController.h"
 #include "NapolitanProject/NPC/Curator/ChaseStatue.h"
+#include "NapolitanProject/YJ/NoteUI/NoteWidget.h"
+#include "NapolitanProject/YJ/NoteUI/NPCInfoWidget.h"
 
 CuratorCompletedCommand::CuratorCompletedCommand(ATestPlayerController* INPC,ATestCharacter* INMainCharacter,APlayerHUD* INPlayerHUD,UWorld* InWorld)
 {
@@ -20,4 +25,25 @@ CuratorCompletedCommand::~CuratorCompletedCommand()
 void CuratorCompletedCommand::Execute()
 {
 	
+	PC->StartEndNPCDialougue(false);
+	
+	MainCharacter->SetPlayerState(EPlayerState::UI);
+
+	// 시간지연
+	FTimerHandle UITimer;
+
+	World->GetTimerManager().SetTimer(UITimer,[this]()
+	{
+		PlayerHUD->NoteUI->SetVisibility(ESlateVisibility::Visible);
+
+		PlayerHUD->NoteUI->OnClickBtn_Btn_Curator();
+		
+	},2.0f,false);
+
+	FTimerHandle UITimer2;
+
+	World->GetTimerManager().SetTimer(UITimer2,[this]()
+	{
+		PlayerHUD->NoteUI->WBP_NPCInfo->SetForcus_ScrollBox_Curator(2,2);
+	},2.5f,false);
 }
