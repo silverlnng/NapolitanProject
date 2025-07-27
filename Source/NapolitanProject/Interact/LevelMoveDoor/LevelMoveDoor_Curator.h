d0d08d1f19618c8e38179c156760bd8368d8a9a0
yjkwak <silverlnng0721@gmail.com>
2025-07-27 20:12:13 +0900

    levelMovedoor 별도로 만들기

---
+++ Source/NapolitanProject/Interact/LevelMoveDoor/LevelMoveDoor_Curator.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "LevelMoveDoor.h"
+#include "LevelMoveDoor_Curator.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class NAPOLITANPROJECT_API ALevelMoveDoor_Curator : public ALevelMoveDoor
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/NapolitanProject/Interact/LevelMoveDoor/LevelMoveDoor_Spider.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "LevelMoveDoor.h"
+#include "LevelMoveDoor_Spider.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class NAPOLITANPROJECT_API ALevelMoveDoor_Spider : public ALevelMoveDoor
+{
+	GENERATED_BODY()
+	
+};
